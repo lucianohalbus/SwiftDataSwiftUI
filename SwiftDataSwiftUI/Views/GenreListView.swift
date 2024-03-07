31e09b2794ba02b030ce184534e45a5d001fd04c
Luciano <lpuzer@yahoo.com.br>
2024-03-06 21:37:57 -0300

    adding GenreListView and onDelete functionality

--- SwiftDataSwiftUI/Views/GenreListView.swift
+++ SwiftDataSwiftUI/Views/GenreListView.swift
@@ -7,13 +7,47 @@ struct GenreListView: View {
     @Query(sort: \Genre.name)
     private var genres: [Genre]
     
+    @State private var presenteAddNew = false
+    
+    @Environment(\.modelContext) private var context
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(genres) { genre in
-                    Text(genre.name)}
+                    Text(genre.name)
+                }
+                .onDelete(perform: deleteGenre(indexSet:))
+            }
+            .navigationTitle("Literary Genre")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        presenteAddNew.toggle()
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .sheet(isPresented: $presenteAddNew) {
+                        AddNewGenre()
+                            .presentationDetents([.fraction(0.3)])
+                            .interactiveDismissDisabled()
+                    }
+                }
             }
         }
-        .navigationTitle("Literary Genre")
+    }
+    
+    private func deleteGenre(indexSet: IndexSet) {
+        indexSet.forEach { index in
+            let genreToDelete = genres[index]
+            context.delete(genreToDelete)
+            
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
